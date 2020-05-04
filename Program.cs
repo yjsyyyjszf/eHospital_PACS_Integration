7c1b3353e2ec3cec4e8ca127e52a83f52ab3093f
Hamidreza Ghaderi <hamidreza.ghaderi@gmail.com>
2020-05-04 19:53:26 -0400

    Update

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -14,6 +17,31 @@ namespace eHospital_PACS_Integration
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
+
+            string url = "https://YOUR_COMPANY_HERE.beebole-apps.com/api";
+            string data = "{\"service\":\"absence.list\", \"company_id\":3}";
+
+            WebRequest myReq = WebRequest.Create(url);
+            myReq.Method = "POST";
+            myReq.ContentLength = data.Length;
+            myReq.ContentType = "application/json; charset=UTF-8";
+
+            string usernamePassword = "YOUR API TOKEN HERE" + ":" + "x";
+
+            UTF8Encoding enc = new UTF8Encoding();
+
+            myReq.Headers.Add("Authorization", "Basic " + Convert.ToBase64String(enc.GetBytes(usernamePassword)));
+
+
+            using (Stream ds = myReq.GetRequestStream())
+            {
+                ds.Write(enc.GetBytes(data), 0, data.Length);
+            }
+
+
+
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
